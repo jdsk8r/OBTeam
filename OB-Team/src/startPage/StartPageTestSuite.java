8d933128e592230ab71758d8fd622fb2d0052759
Diego Fuentes <jorge.fuentes@sysco.no>
2013-02-27 15:15:35 +0100

    comment of StartPageTestSuite

--- OB-Team/src/startPage/StartPageTestSuite.java
+++ OB-Team/src/startPage/StartPageTestSuite.java
@@ -7,6 +7,9 @@ import junit.framework.TestSuite;
 public class StartPageTestSuite {
 	
 	  public static Test suite() {
+		  
+		  /* this class runs the Test Suite of the Start Page */
+		  
 	        TestSuite suite = new TestSuite("OBTeam Project - Start Page");
 	        suite.addTestSuite(VerifyAccessToModules.class);
 	        suite.addTestSuite(VerifyAccessToOperations.class);
